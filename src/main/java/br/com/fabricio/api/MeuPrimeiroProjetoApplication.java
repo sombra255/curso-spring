b492e498857c7585647f14758a65fe121429ee95
Ramires <fabricio.tidir@gmail.com>
2018-08-27 14:37:30 -0300

    criacao de profiles para dev, test e prod... e criptografia de senhas utilizando o springboot

--- src/main/java/br/com/fabricio/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/br/com/fabricio/api/MeuPrimeiroProjetoApplication.java
@@ -1,13 +1,38 @@
 package br.com.fabricio.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.fabricio.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 		System.out.println("Alteração da mensagem123...");
 	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+//			System.out.println("Elementos por página = "+qtdPorPagina);
+			String senhaEncoded = SenhaUtils.geraBCrypt("123456");
+			System.out.println("Senha encoded:" + senhaEncoded);
+			
+			senhaEncoded = SenhaUtils.geraBCrypt("123456");
+			System.out.println("Senha encoded:" + senhaEncoded);
+			
+			System.out.println("Senha válida = " + SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
+	}
+	
+	
+	
 }
